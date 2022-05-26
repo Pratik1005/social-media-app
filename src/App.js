b8e46eeae2dfeae409690b2916d15b42f251f571
Pratik Devle <pratik.devle98@gmail.com>
2022-05-26 13:06:06 +0530

    installed chakra UI

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
+  Grid,
+  theme,
+} from '@chakra-ui/react';
+
+function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <h1>Hello</h1>
+    </ChakraProvider>
+  );
+}
+
+export default App;
